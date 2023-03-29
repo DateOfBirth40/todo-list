ffec40b8b1da129ce583c14caabfd3b33e14b979
Derek O'Brien <derekobrien40@gmail.com>
2023-03-29 18:21:32 -0400

    Edit event listeners for separated functions from dom.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 /* eslint-disable max-len */
 import './style.css';
-import {createTodoElement, completeTask, todoList} from './DOM.js';
+import {createTaskObject, createTaskElement, completeTask, todoList, addTask} from './DOM.js';
 
-const todoFactory = (title, description, dueDate, notes, checklist) => {
-  return {title, description, dueDate, notes, checklist};
+const todoFactory = (title, dueDate, dueTime, notes, checklist) => {
+  return {title, dueDate, dueTime, notes, checklist};
 };
 
 const addTodoBtn = document.querySelector('#add-todo');
-addTodoBtn.addEventListener('click', createTodoElement);
+addTodoBtn.addEventListener('click', createTaskObject);
 
 todoList.addEventListener('click', function(event) {
   if (event.target.classList.contains('check-btn')) {
     completeTask(event);
   }
 });
+
+addTask.addEventListener('click', createTaskElement);
