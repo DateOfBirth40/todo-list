0b3323438d5d335cda15542e6ce82a95daecad16
Derek O'Brien <derekobrien40@gmail.com>
2023-04-03 14:12:47 -0400

    Add createCategory(); add reset functionality to forms after submission

--- src/DOM.js
+++ src/DOM.js
@@ -3,34 +3,38 @@ import {todoFactory} from './index.js';
 
 const todoList = document.querySelector('#todo-list');
 const taskForm = document.querySelector('#task-form');
+const categoryForm = document.querySelector('#category-form');
 
-function displayForm(form) {
-  form.style.display = 'block';
+function displayElement(element) {
+  element.style.display = 'block';
 }
 
-function hideForm(form) {
-  form.style.display = 'none';
+function hideElement(element) {
+  element.style.display = 'none';
 }
 
 function getFormData(form) {
   const title = document.querySelector('#title').value;
+  const category = document.querySelector('#category').value;
   const date = document.querySelector('#date').value;
   const time = document.querySelector('#time').value;
   const notes = document.querySelector('#notes').value;
-  const newTask = todoFactory(title, date, time, notes, false);
+  const newTask = todoFactory(title, category, date, time, notes, false);
   console.log(newTask);
   return newTask;
 }
 
 function createTaskElement() {
+  // Turn these into a flex div that shows title, date, category, priority
   const li = document.createElement('li');
   const check = document.createElement('div');
   check.classList.add('check-btn', 'unchecked');
   const formData = getFormData(taskForm);
   li.textContent = formData.title;
   li.prepend(check);
-  hideForm(taskForm);
+  hideElement(taskForm);
   todoList.appendChild(li);
+  taskForm.reset();
 }
 
 function completeTask(event) {
@@ -46,11 +50,27 @@ function completeTask(event) {
   }
 }
 
+// Create a dropdown of the category list items that is prompted
+// when creating a new task
+function createCategory() {
+  const categoryList = document.querySelector('#category-list');
+  const newCat = document.createElement('li');
+  newCat.textContent = document.querySelector('#category').value;
+  categoryList.appendChild(newCat);
+  hideElement(categoryForm);
+  categoryForm.reset();
+}
+
+// Add an X button next to all list items to allow them to be deleted
+
 export {
   createTaskElement,
   getFormData,
   completeTask,
-  displayForm,
+  createCategory,
+  displayElement,
+  hideElement,
   taskForm,
+  categoryForm,
   todoList,
 };
