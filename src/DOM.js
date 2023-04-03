0f7771761ba0e5bd0f0174c44c2dc22b33bedbc5
Derek O'Brien <derekobrien40@gmail.com>
2023-04-03 17:05:39 -0400

    Active list title adds to header; does not work with lists added through DOM yet

--- src/DOM.js
+++ src/DOM.js
@@ -1,12 +1,17 @@
+/* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
-import {todoFactory} from './index.js';
+import {todoFactory, createNewTaskBtn, displayCategoryFormBtn} from './index.js';
 
 const todoList = document.querySelector('#todo-list');
 const taskForm = document.querySelector('#task-form');
 const categoryForm = document.querySelector('#category-form');
 
-function displayElement(element) {
+function displayBlock(element) {
   element.style.display = 'block';
+}
+
+function displayGrid(element) {
+  element.style.display = 'grid';
 }
 
 function hideElement(element) {
@@ -35,6 +40,7 @@ function createTaskElement() {
   hideElement(taskForm);
   todoList.appendChild(li);
   taskForm.reset();
+  displayBlock(createNewTaskBtn);
 }
 
 function completeTask(event) {
@@ -52,25 +58,46 @@ function completeTask(event) {
 
 // Create a dropdown of the category list items that is prompted
 // when creating a new task
+
 function createCategory() {
   const categoryList = document.querySelector('#category-list');
+  const catInput = document.querySelector('#category').value;
   const newCat = document.createElement('li');
-  newCat.textContent = document.querySelector('#category').value;
+  newCat.textContent = catInput;
   categoryList.appendChild(newCat);
   hideElement(categoryForm);
   categoryForm.reset();
 }
 
+// Adds the active list title to the header
+// and assigns .active to the clicked element
+function addActiveListToHeader() {
+  const listItems = document.querySelectorAll('#sidebar li');
+  listItems.forEach((item) => {
+    item.addEventListener('click', function(e) {
+      const activeListHeader = document.querySelector('#active-list-title');
+      activeListHeader.textContent = item.textContent;
+      listItems.forEach(function(item) {
+        item.classList.remove('active');
+      });
+      e.target.classList.add('active');
+    });
+  });
+}
+
+addActiveListToHeader();
+
 // Add an X button next to all list items to allow them to be deleted
 
 export {
   createTaskElement,
-  getFormData,
   completeTask,
   createCategory,
-  displayElement,
+  displayBlock,
+  displayGrid,
   hideElement,
   taskForm,
   categoryForm,
+  // catInput,
   todoList,
 };
